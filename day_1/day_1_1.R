36afe019744e24ff05fb8b0587260fa9b240417e
majpark21 <jacques@ebi.ac.uk>
2023-12-01 20:24:12 +0000

    day1_1

---
+++ day_1/day_1_1.R
@@ -0,0 +1,16 @@
+library(stringr)
+library(data.table)
+
+fi_in <- "./day_1/input_1.txt"
+dt <- fread(fi_in, header = F)
+dt <- dt[V1 != ""]
+
+regex_first <- "^.*?(\\d{1})"
+regex_second <- "(\\d{1})(?!.*\\d)"
+
+dt[, first := str_extract(V1, regex_first, group=1)]
+dt[, second := str_extract(V1, regex_second, group=1)]
+dt[, third := as.numeric(paste0(first, second))]
+
+out <- dt[, sum(third)]
+cat(out)
