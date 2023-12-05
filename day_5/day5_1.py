9d39106b09906fde9a73a32203e320faa29813c2
majpark21 <jacques@ebi.ac.uk>
2023-12-05 15:15:22 +0000

    day 5 1 optimized and correct

--- day_5/day5_1.py
+++ day_5/day5_1.py
@@ -1,11 +1,43 @@
 import re
-import pandas as pd
-import numpy as np
 from collections import OrderedDict
-from functools import reduce
 
-in_fi = "./day_5/input5_1.txt"
-# in_fi = "./day_5/example5_1.txt"
+class MapRange:
+  def __init__(self, start_destination, start_source, length):
+    self.start_destination = start_destination
+    self.start_source = start_source
+    self.length = length
+
+  def __str__(self):
+    return f"Range[{self.start_destination}, {self.start_source}, {self.length}]"
+  
+  def __repr__(self):
+    return self.__str__()
+
+  def get_destinations(self):
+    return list(range(self.start_destination, self.start_destination + self.length))
+  
+  def get_sources(self):
+    return list(range(self.start_source, self.start_source + self.length))
+
+  def isin_destinations(self, dest):
+    return self.start_destination <= dest < self.start_destination + self.length
+  
+  def isin_sources(self, source):
+    return self.start_source <= source < self.start_source + self.length
+  
+  def get_destination(self, source):
+    if self.isin_sources(source):
+      diff = source - self.start_source
+      return self.start_destination + diff
+    else:
+      return None
+
+# tmp = MapRange(50, 98, 2)
+# tmp.isin_sources(98)
+# tmp.get_destination(100)
+
+in_fi = "./input5_1.txt"
+# in_fi = "./example5_1.txt"
 with open(in_fi) as f:
   lines = f.readlines()
 lines = [line.strip("\n") for line in lines]
@@ -22,45 +54,26 @@ for item in lines:
   if item == "":
     continue
   elif re.search("\d", item) is not None:
-    d_all_maps[last_key].append([int(i) for i in item.split(" ")])
+    l_range = [int(i) for i in item.split(" ")]
+    d_all_maps[last_key].append(MapRange(start_destination=l_range[0], start_source=l_range[1], length=l_range[2]))
   else:
     last_key = item.rstrip(" map:")
     d_all_maps[last_key] = []
 order_maps = list(d_all_maps.keys())  # Ordered by order of reading
 
-# Format the maps into dataframes/arrays with source-destination ranges
-for key_map in order_maps:
-  key_source, key_dest = key_map.split("-to-")
-  l_df = []
-  for val_map in d_all_maps[key_map]:
-    start_dest, start_source, length = val_map
-    d_map = {
-      "source": np.arange(start_source, start_source + length),
-      "dest": np.arange(start_dest, start_dest + length)
-    }
-    df_map = pd.DataFrame.from_dict(d_map)
-    l_df.append(df_map)
-  d_all_maps[key_map] = pd.concat(l_df)
 
-def reduceMerge(x,y):
-  out = pd.merge(x, y, left_on="dest", right_on="source", how="left")
-  # Numbers that were not mapped are mapped on themselves
-  out.loc[pd.isna(out.source_y), "dest_y"] = out.loc[pd.isna(out.source_y), "dest_x"]
-  out = out.drop(["source_x", "dest_x"], axis = 1)
-  out.columns = ["source", "dest"]
-  # Left Merge is turning into float
-  out.dest = out.dest.astype(int)
-  return out
-  
-# out= pd.merge(d_all_maps[order_maps[0]], d_all_maps[order_maps[1]], left_on="dest", right_on="source", how="left")
-l_out = {}
+d_out = {}
 for seed in seeds:
-  if seed in list(d_all_maps[order_maps[0]].source):
-    df_seed = d_all_maps[order_maps[0]].loc[d_all_maps[order_maps[0]].source == seed]
-  else:
-    df_seed = pd.DataFrame({"source": [seed], "dest": [seed]})
-  seed_to_location = reduce(reduceMerge, [df_seed] + [d_all_maps[k] for k in order_maps[1:]])
-  l_out[str(seed)] = int(seed_to_location.dest)
+  soil = [irange.get_destination(seed) for irange in d_all_maps['seed-to-soil']]
+  soil = next((dest for dest in soil if dest is not None), seed) # First non None otherwise itself
 
-out = min(l_out.values())
+  current_coord = soil
+  for key_map in order_maps[1:]: # skip seed to soil
+    new_coord = [irange.get_destination(current_coord) for irange in d_all_maps[key_map]]
+    new_coord = next((dest for dest in new_coord if dest is not None), current_coord) # First non None otherwise itself
+    current_coord = new_coord
+
+  d_out[str(seed)] = current_coord
+
+out = min(d_out.values())
 print(out)
