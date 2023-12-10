83b47358d745f0644805a52aef39f2bc9434eb44
majpark21 <jacques@ebi.ac.uk>
2023-12-10 12:01:35 +0000

    day 9_2

---
+++ day_9/day9_2.R
@@ -0,0 +1,48 @@
+library(stringr)
+# in_fi <- "day_9/example9.txt"
+in_fi <- "day_9/input_9_1.txt"
+
+inputs <- readLines(in_fi)
+inputs <- lapply(inputs, function(x){as.numeric(unlist(str_split(x, " ")))})
+
+prependList <- function(x, new_val){
+  out <- c(new_val, x)
+  return(out)
+}
+
+getDiffs <- function(x, out=list()){
+  # Add initial vector
+  if(length(out)==0){
+    out[[1]] <- x
+  }
+  if(all(x==0)){
+    return(out)
+  } else {
+    diffs <- diff(x)
+    out[[length(out) + 1]] <- diffs
+    getDiffs(diffs, out)
+  }
+}
+
+predictFirst <- function(l_diffs, curr_lvl=length(l_diffs)){
+  last_lvl <- length(l_diffs)
+  if(curr_lvl==0){
+    # First of first lvl
+    return(l_diffs[[1]][1])
+  } else if(curr_lvl==last_lvl) {
+    new_val <- 0
+  } else {
+    # First from current level + first from level below
+    new_val <- l_diffs[[curr_lvl]][1] - l_diffs[[curr_lvl+1]][1]
+    # new_val <- l_diffs[[curr_lvl]][length(l_diffs[[curr_lvl]])] + l_diffs[[curr_lvl+1]][length(l_diffs[[curr_lvl+1]])]
+  }
+  # Prepend value to current level
+  l_diffs[[curr_lvl]] <- c(new_val, l_diffs[[curr_lvl]])
+  predictFirst(l_diffs, curr_lvl = curr_lvl-1)
+}
+
+
+l_diffs <- lapply(inputs, getDiffs)
+l_preds <- lapply(l_diffs, predictFirst)
+out <- sum(unlist(l_preds))
+cat(out)
